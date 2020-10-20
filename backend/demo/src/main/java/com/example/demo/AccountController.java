cf75b4f70cd4990c7512215410263e1debdadf2d
Montilus <Kenny.Montilus3@T-Mobile.com>
2020-10-20 10:54:00 -0400

    possibly fixed CORS issue

--- backend/demo/src/main/java/com/example/demo/AccountController.java
+++ backend/demo/src/main/java/com/example/demo/AccountController.java
@@ -9,11 +9,13 @@ public class AccountController {
     @Autowired
     AccountService accountService;
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/accounts/{name}")
     private Account getAccount(@PathVariable("name") String name){
         return accountService.getAccount(name);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PutMapping("/accounts/{name}")
     public Account updateAccount(@PathVariable("name") String name, @RequestBody Account account){
         return accountService.updateAccount(name, account);
